6462abe82eef1f6edb1985daf8ce6c4601561ee2
Po Yu Chen <pyshif@gmail.com>
2022-09-11 02:45:20 +0800

    add Layout.tsx

--- src/pages/Layout/index.tsx
+++ src/pages/Layout/index.tsx
@@ -1,20 +1,21 @@
 import { Outlet } from 'react-router-dom';
 import Navbar from 'components/Navbar';
 import Footer from 'components/Footer';
+import './index.css';
 
 function Layout() {
     return (
-        <>
-            <header>
+        <div className="grid grid-cols-1 grid-rows-[auto_minmax(0,_1fr)_auto] min-h-screen">
+            <header className="col-span-1 row-auto">
                 <Navbar />
             </header>
-            <main>
+            <main className="bg-white">
                 <Outlet />
             </main>
-            <footer>
+            <footer className="bg-amber-500">
                 <Footer />
             </footer>
-        </>
+        </div>
     );
 }
 
