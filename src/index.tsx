fda1e86b3775b6f4c910b478f9407db6597465b8
Po Yu Chen <pyshif@gmail.com>
2022-11-02 19:44:39 +0800

    add redux store and append to index

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,14 @@
 import { createRoot } from 'react-dom/client';
+import store from 'store';
+import { Provider } from 'react-redux';
 import App from './App';
-
 
 const element = document.querySelector('#root') as HTMLDivElement;
 if (element) {
     const root = createRoot(element);
-    root.render(<App/>);
+    root.render(
+        <Provider store={store}>
+            <App />
+        </Provider>
+    );
 }
---
+++ src/store/index.ts
@@ -0,0 +1,22 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { TypedUseSelectorHook, useSelector, useDispatch } from 'react-redux';
+// slice
+import authReducer from './features/authSlice';
+
+// store
+const store = configureStore({
+    reducer: {
+        auth: authReducer
+    }
+});
+
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
+
+export const useAppDispatch: () => AppDispatch = useDispatch;
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+
+
+
+export default store;
