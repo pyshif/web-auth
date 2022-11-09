8ba9e0742756316bbcd3475b8dd740b421c8b926
Po Yu Chen <pyshif@gmail.com>
2022-11-09 20:15:33 +0800

    adjust authSlice thunk function structure

--- src/store/features/authSlice.ts
+++ src/store/features/authSlice.ts
@@ -48,222 +48,160 @@ const initialState: StateAuth = {
     },
 };
 
+// thunk function -> to generate payload from server, so just focus on return payload.
+
 export const apiHealth = createAsyncThunk<any>('auth/health', async () => {
-    try {
-        const response = await api.v1.auth.health();
-        // console.log('response :>> ', response);
-        // return response;
-        return {
-            status: response.status,
-            data: response.data
-        }
-    } catch (error) {
-        // console.log('error :>> ', error);
-        return error;
+    const response = await api.v1.auth.health();
+    // console.log('response :>> ', response);
+    // return response;
+
+    return {
+        status: response.status,
+        data: response.data
     }
 });
 
 // sign-up
 export const apiSignUp = createAsyncThunk<any, DataSignUp>('auth/signUp', async (data) => {
-    try {
-        const response = await api.v1.auth.signUp(data) as unknown as any;
-        return {
-            status: response.status,
-        };
-    } catch (error) {
-        return error;
-    }
+    const response = await api.v1.auth.signUp(data) as unknown as any;
+    return {
+        status: response.status,
+    };
 });
 // sign-in 
 export const apiSignIn = createAsyncThunk<any, DataSignIn>('auth/signIn', async (data) => {
-    try {
-        const response = await api.v1.auth.signIn(data) as unknown as any;
-        const { accessToken } = response.data;
-        if (!accessToken) {
-            throw new Error('Invalid access-token!');
-        }
-        const user = decodeJWT(accessToken) as any;
-        return {
-            status: response.status,
-            token: accessToken,
-            user: {
-                name: user.name,
-                birthday: user.birthday,
-                phone: user.phone,
-                gender: user.gender,
-                avatar: user.avatar,
-                email: user.email,
-            }
-        };
-    } catch (error) {
-        return error;
-
+    const response = await api.v1.auth.signIn(data) as unknown as any;
+    const { accessToken } = response.data;
+    if (!accessToken) {
+        throw new Error('Invalid access-token!');
     }
+    const user = decodeJWT(accessToken) as any;
+    return {
+        status: response.status,
+        token: accessToken,
+        user: {
+            name: user.name,
+            birthday: user.birthday,
+            phone: user.phone,
+            gender: user.gender,
+            avatar: user.avatar,
+            email: user.email,
+        }
+    };
 });
 
 // google
 export const apiGoogleSignIn = createAsyncThunk<any, string>('auth/google', async (googleIDToken) => {
-    try {
-        const response = await api.v1.auth.googleSignIn(googleIDToken) as unknown as any;
+    const response = await api.v1.auth.googleSignIn(googleIDToken) as unknown as any;
 
-        return response;
-    } catch (error) {
-        return error;
-    }
+    return response;
 });
 
 // token
 export const apiValidateToken = createAsyncThunk<any, string>('auth/validateToken', async (accessToken) => {
-    try {
-        const response = await api.v1.auth.validateToken(accessToken) as unknown as any;
-        return {
-            status: response.status
-        }
-    } catch (error) {
-        return error;
+    const response = await api.v1.auth.validateToken(accessToken) as unknown as any;
+    return {
+        status: response.status
     }
 });
 
 export const apiRequestToken = createAsyncThunk<any>('auth/requestToken', async () => {
-    try {
-        const response = await api.v1.auth.requestToken() as unknown as any;
-        const { accessToken } = response.data;
-        if (!accessToken) {
-            throw new Error('Invalid access-token!');
-        }
-        const user = decodeJWT(accessToken) as any;
-
-        return {
-            status: response.status,
-            token: accessToken,
-            user: {
-                name: user.name,
-                birthday: user.birthday,
-                phone: user.phone,
-                gender: user.gender,
-                avatar: user.avatar,
-                email: user.email
-            }
-        };
-    } catch (error) {
-        return error;
+    const response = await api.v1.auth.requestToken() as unknown as any;
+    const { accessToken } = response.data;
+    console.log('accessToken :>> ', accessToken);
+    if (!accessToken) {
+        throw new Error('Invalid access-token!');
     }
+    const user = decodeJWT(accessToken) as any;
+
+    return {
+        status: response.status,
+        token: accessToken,
+        user: {
+            name: user.name,
+            birthday: user.birthday,
+            phone: user.phone,
+            gender: user.gender,
+            avatar: user.avatar,
+            email: user.email
+        }
+    };
 });
 
 // sign-out
 export const apiSignOut = createAsyncThunk<any>('auth/signOut', async () => {
-    try {
-        const response = await api.v1.auth.signOut() as unknown as any;
-        return {
-            status: response.status
-        };
-    } catch (error) {
-        return error;
-    }
+    const response = await api.v1.auth.signOut() as unknown as any;
+    return {
+        status: response.status
+    };
 });
 
 // forgot
 export const apiForgotPassword = createAsyncThunk<any, DataForgot>('auth/forgot', async (data) => {
-    try {
-        const response = await api.v1.auth.forgotPassword(data) as unknown as any;
-        return {
-            status: response.status
-        };
-    } catch (error) {
-        return error;
-    }
+    const response = await api.v1.auth.forgotPassword(data) as unknown as any;
+    return {
+        status: response.status
+    };
 });
 
 export const apiResetPasswordByLink = createAsyncThunk<any, { linkToken: string, data: DataResetPasswordByLink }>('auth/resetPasswordByLink', async ({ linkToken, data }) => {
-    try {
-        const response = await api.v1.auth.resetPasswordByLink(linkToken, data) as unknown as any;
-        return {
-            status: response.status
-        };
-    } catch (error) {
-        return error;
-    }
+    const response = await api.v1.auth.resetPasswordByLink(linkToken, data) as unknown as any;
+    return {
+        status: response.status
+    };
 });
 
 // reset
 export const apiResetPassword = createAsyncThunk<any, { accessToken: string, data: DataResetPassword }>('auth/resetPassword', async ({ accessToken, data }) => {
-    try {
-        const response = await api.v1.auth.ResetPassword(accessToken, data) as unknown as any;
-        return {
-            status: response.status
-        };
-    } catch (error) {
-        return error;
-    }
+    const response = await api.v1.auth.ResetPassword(accessToken, data) as unknown as any;
+    return {
+        status: response.status
+    };
 });
 
 
 // user
 export const apiUpdateUserName = createAsyncThunk<any, { accessToken: string, name: string }>('auth/updateUserName', async ({ accessToken, name }) => {
-    try {
-        const response = await api.v1.auth.updateUserName(accessToken, name) as unknown as any;
-        return {
-            status: response.status
-        };
-    } catch (error) {
-        return error;
-    }
+    const response = await api.v1.auth.updateUserName(accessToken, name) as unknown as any;
+    return {
+        status: response.status
+    };
 });
 
 export const apiUpdateUserBirthday = createAsyncThunk<any, { accessToken: string, birthday: string }>('auth/updateUserBirthday', async ({ accessToken, birthday }) => {
-    try {
-        const response = await api.v1.auth.updateUserBirthday(accessToken, birthday) as unknown as any;
-        return {
-            status: response.status
-        };
-    } catch (error) {
-        return error;
-    }
+    const response = await api.v1.auth.updateUserBirthday(accessToken, birthday) as unknown as any;
+    return {
+        status: response.status
+    };
 });
 
 export const apiUpdateUserPhone = createAsyncThunk<any, { accessToken: string, phone: string }>('auth/updateUserPhone', async ({ accessToken, phone }) => {
-    try {
-        const response = await api.v1.auth.updateUserPhone(accessToken, phone) as unknown as any;
-        return {
-            status: response.status
-        };
-    } catch (error) {
-        return error;
-    }
+    const response = await api.v1.auth.updateUserPhone(accessToken, phone) as unknown as any;
+    return {
+        status: response.status
+    };
 });
 
 export const apiUpdateUserGender = createAsyncThunk<any, { accessToken: string, gender: 'male' | 'female' }>('auth/updateUserGender', async ({ accessToken, gender }) => {
-    try {
-        const response = await api.v1.auth.updateUserGender(accessToken, gender) as unknown as any;
-        return {
-            status: response.status
-        };
-    } catch (error) {
-        return error;
-    }
+    const response = await api.v1.auth.updateUserGender(accessToken, gender) as unknown as any;
+    return {
+        status: response.status
+    };
 });
 
 export const apiUpdateUserEmail = createAsyncThunk<any, { accessToken: string, email: string }>('auth/updateUserEmail', async ({ accessToken, email }) => {
-    try {
-        const response = await api.v1.auth.updateUserEmail(accessToken, email) as unknown as any;
-        return {
-            status: response.status
-        };
-    } catch (error) {
-        return error;
-    }
+    const response = await api.v1.auth.updateUserEmail(accessToken, email) as unknown as any;
+    return {
+        status: response.status
+    };
 });
 
 
 export const apiUpdateUserAvatar = createAsyncThunk<any, { accessToken: string, data: DataUpdateUserAvatar }>('auth/updateUserAvatar', async ({ accessToken, data }) => {
-    try {
-        const response = await api.v1.auth.updateUserAvatar(accessToken, data) as unknown as any;
-        return {
-            status: response.status
-        };
-    } catch (error) {
-        return error;
-    }
+    const response = await api.v1.auth.updateUserAvatar(accessToken, data) as unknown as any;
+    return {
+        status: response.status
+    };
 });
 
 // slice
