e56579d47499bb3331290ac0c3b215deda21b425
Po Yu Chen <pyshif@gmail.com>
2022-12-08 00:29:19 +0800

    complete app router testing

--- src/__tests__/app.test.js
+++ src/__tests__/app.test.js
@@ -45,7 +45,7 @@ describe('Router Testing', () => {
         const navEl = await screen.findByText(/^colorful$/);
         expect(navEl).toBeInTheDocument();
 
-        const footEl = await screen.findByText(/what is this project/i);
+        const footEl = await screen.findByText(/^what is this project \?$/i);
         expect(footEl).toBeInTheDocument();
     });
 
@@ -57,7 +57,7 @@ describe('Router Testing', () => {
         const navEl = await screen.findByText(/^colorful$/);
         expect(navEl).toBeInTheDocument();
 
-        const footEl = await screen.findByText(/what is this project/i);
+        const footEl = await screen.findByText(/^what is this project \?$/i);
         expect(footEl).toBeInTheDocument();
     });
 
@@ -69,7 +69,7 @@ describe('Router Testing', () => {
         const navEl = await screen.findByText(/^colorful$/);
         expect(navEl).toBeInTheDocument();
 
-        const footEl = await screen.findByText(/what is this project/i);
+        const footEl = await screen.findByText(/^what is this project \?$/i);
         expect(footEl).toBeInTheDocument();
     });
 
@@ -81,54 +81,52 @@ describe('Router Testing', () => {
         const navEl = await screen.findByText(/^colorful$/);
         expect(navEl).toBeInTheDocument();
 
-        const footEl = await screen.findByText(/what is this project/i);
+        const footEl = await screen.findByText(/^what is this project \?$/i);
         expect(footEl).toBeInTheDocument();
     });
 
     it('should render reset password page', async () => {
         renderWithReduxAndRouter(<App />, routes.auth.reset.self);
-        const titleEl = await waitFor(() => screen.getByText(/reset your password in 30 mins/i));
+        const titleEl = await waitFor(() => screen.getByText(/^reset your password in 30 mins$/i));
         expect(titleEl).toBeInTheDocument();
 
         const navEl = await screen.findByText(/^colorful$/);
         expect(navEl).toBeInTheDocument();
 
-        const footEl = await screen.findByText(/what is this project/i);
+        const footEl = await screen.findByText(/^what is this project \?$/i);
         expect(footEl).toBeInTheDocument();
     });
 
     it('should render reset password by link page (same with reset page)', async () => {
         renderWithReduxAndRouter(<App />, reverse(routes.auth.reset.resetId, { resetId: 'token' }));
-        const titleEl = await waitFor(() => screen.getByText(/reset your password in 30 mins/i));
+        const titleEl = await waitFor(() => screen.getByText(/^reset your password in 30 mins$/i));
         expect(titleEl).toBeInTheDocument();
 
         const navEl = await screen.findByText(/^colorful$/);
         expect(navEl).toBeInTheDocument();
 
-        const footEl = await screen.findByText(/what is this project/i);
+        const footEl = await screen.findByText(/^what is this project \?$/i);
         expect(footEl).toBeInTheDocument();
     });
 
     it('should render user profile page', async () => {
         renderWithReduxAndRouter(<App />, routes.user);
-        const titleEl = await waitFor(() => screen.getByText(/update/i));
+        const titleEl = await waitFor(() => screen.getByText(/^update$/i));
         expect(titleEl).toBeInTheDocument();
 
         const navEl = await screen.findByText(/^colorful$/);
         expect(navEl).toBeInTheDocument();
 
-        const footEl = await screen.findByText(/what is this project/i);
+        const footEl = await screen.findByText(/^what is this project \?$/i);
         expect(footEl).toBeInTheDocument();
     });
 
     it('should render navbar and link can work', async () => {
         // render
         const { user, findByText, findAllByText } = renderWithReduxAndRouter(<App />, routes.home);
-        // mock scroll to top
-        // const spyInsertBefore = jest.spyOn(container, 'insertBefore').mockImplementation(jest.fn());
         // make sure render complete
         await waitFor(() => expect(screen.getByText(/^jwt authentication$/i)).toBeInTheDocument());
-        // get navbar element
+        // get links
         const brandLink = await findByText(/^colorful$/);
         const homeLinks = await findAllByText(/^home$/i);
         const authLinks = await findAllByText(/^auth$/i);
@@ -145,4 +143,65 @@ describe('Router Testing', () => {
         await user.click(brandLink);
         await waitFor(() => expect(screen.getByText(/^jwt authentication$/i)).toBeInTheDocument());
     });
+
+    it('should render footer and webmap can work', async () => {
+        // render
+        const { user, findByText, findAllByText } = renderWithReduxAndRouter(<App />, routes.home);
+        await waitFor(() => expect(screen.getByText(/^jwt authentication$/i)).toBeInTheDocument());
+        // get links from footer
+        const homeLink = await findByText(/^introduction$/i);
+        const userLink = await findByText(/^user profile$/i);
+        const signInLink = await findByText(/^sign in$/i);
+        const signUpLink = await findByText(/^sign up$/i);
+        const forgotLink = await findByText(/^forgot password$/i);
+        const resetLink = await findByText(/^reset password$/i);
+        // console.log('homeLink :>> ', homeLink);
+        // console.log('userLink :>> ', userLink);
+        // console.log('signInLink :>> ', signInLink);
+        // console.log('signUpLink :>> ', signUpLink);
+        // console.log('forgotLink :>> ', forgotLink);
+        // console.log('resetLink :>> ', resetLink);
+        // navigate to user profile page
+        const mockScroll = jest.spyOn(document, 'querySelector');
+        mockScroll.mockReturnValueOnce({ scroll: jest.fn((x, y) => '') })
+        await user.click(userLink);
+        await waitFor(() => expect(screen.getByText(/^update$/i)).toBeInTheDocument());
+        // console.log('window.location.href :>> ', window.location.href);
+        // navigate to sign-in page
+        mockScroll.mockReturnValueOnce({ scroll: jest.fn((x, y) => '') })
+        await user.click(signInLink);
+        await waitFor(() => expect(screen.getByText(/^sign in to your account$/i)).toBeInTheDocument());
+        // console.log('window.location.href :>> ', window.location.href);
+        // navigate to sign-up page
+        mockScroll.mockReturnValueOnce({ scroll: jest.fn((x, y) => '') })
+        await user.click(signUpLink);
+        await waitFor(() => expect(screen.getByText(/^sign up your account$/i)).toBeInTheDocument());
+        // console.log('window.location.href :>> ', window.location.href);
+        // navigate to forgot page
+        mockScroll.mockReturnValueOnce({ scroll: jest.fn((x, y) => '') })
+        await user.click(forgotLink);
+        await waitFor(() => expect(screen.getByText(/^get your reset password link$/i)).toBeInTheDocument());
+        // console.log('window.location.href :>> ', window.location.href);
+        // navigate to reset page
+        mockScroll.mockReturnValueOnce({ scroll: jest.fn((x, y) => '') })
+        await user.click(resetLink);
+        await waitFor(() => expect(screen.getByText(/^reset your password in 30 mins$/i)).toBeInTheDocument());
+        // console.log('window.location.href :>> ', window.location.href);
+        // navigate to home
+        mockScroll.mockReturnValueOnce({ scroll: jest.fn((x, y) => '') });
+        await user.click(homeLink);
+        await waitFor(() => expect(screen.getByText(/^jwt authentication$/i)).toBeInTheDocument());
+        // console.log('window.location.href :>> ', window.location.href);
+        // console.log('mockScroll.mock.calls.length :>> ', mockScroll.mock.calls.length);
+    });
 });
+
+describe('Sign Up Flow Testing', () => { });
+
+describe('Sign In Flow Testing', () => { });
+
+describe('Forgot Password Testing', () => { });
+
+describe('Reset Password Testing', () => { });
+
+describe('User Profile Updating Testing', () => { });
